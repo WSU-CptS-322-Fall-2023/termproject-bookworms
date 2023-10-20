38fd15e1049c109e611e8129f88f76c9a9adf0e0
Tyler Lighthall <tyler.lighthall@wsu.edu>
2023-10-20 00:00:13 -0700

    Tyler L: added login and registration for regular users and admin users. Admin users will be designated on the homepage with :Admin after their username

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from app import db
+from app import db, login
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,17 +14,43 @@ class Review(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     likes = db.Column(db.Integer, default = 0)
 
-class User(UserMixin): 
+class User(UserMixin, db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
     email = db.Column(db.String(120))
     password_hash = db.Column(db.String(128))
+    user_type = db.Column(db.String(50))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'User',
+        'polymorphic_on': user_type
+    }
 
     def __repr__(self):
-        return '<User {} - {} {} - {};>'.format(self.id, self.username, self.email)
+        return '<User {} - {} - {};>'.format(self.id, self.username, self.email)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def get_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+class Admin(User):
+    __tablename__='admin'
+    id = db.Column(db.ForeignKey("user.id"), primary_key = True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'Admin'
+    }
+
+class Regular_User(User):
+    __tablename__='reg_user'
+    id = db.Column(db.ForeignKey("user.id"), primary_key = True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'Reg_User'
+    }
+
